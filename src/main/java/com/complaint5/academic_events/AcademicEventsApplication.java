37a373947f8226648de4422cf33b8a272b932a9f
Complaint5 <phelipe200490@gmail.com>
2023-06-09 09:28:54 -0300

    Spring Boot project

---
+++ src/main/java/com/complaint5/academic_events/AcademicEventsApplication.java
@@ -0,0 +1,13 @@
+package com.complaint5.academic_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicEventsApplication.class, args);
+	}
+
+}
